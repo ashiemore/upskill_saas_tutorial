a0f34c41af0a66dc39d260295edc7702de4d42cd
Sophia Foreman <sophiex352@gmail.com>
2017-03-25 20:42:21 +0000

    Did stuff with error messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-    
+  validates :name, presence: true 
+  validates :email, presence: true
+  validates :comments, presence: true, length: { maximum: 500, message: 'must be no longer than 500 characters.' }
 end
